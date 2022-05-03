26ee3006054e0d0bf527036d14fff1dbb0ecad58
Guk Dmitry <gukdmitry.s@gmail.com>
2022-05-03 03:40:44 +0300

    fix mobile version

--- src/components/MainPage/Swiper/SwiperSLider.jsx
+++ src/components/MainPage/Swiper/SwiperSLider.jsx
@@ -10,7 +10,7 @@ export default class SimpleSlider extends Component {
             infinite: true,
             speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToScroll: 1
         }
         const casesArray = [
             {
