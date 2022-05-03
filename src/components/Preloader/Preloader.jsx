b1c14c95dd74c07edbb0b1e66c82c0356dc43570
Guk Dmitry <gukdmitry.s@gmail.com>
2022-05-04 00:24:10 +0300

    fix

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -6,7 +6,7 @@ import preloader_mp from './X_A2.mp4'
 const Preloader = ({ isLoading }) => {
         return (
             <div>
-                <video className="preloader" autoPlay muted>
+                <video className="preloader" autoPlay muted playsinline>
                     <source src={preloader_mp} type="video/mp4" />
                     <source src={preloader} type="video/webm" />
                 </video>
